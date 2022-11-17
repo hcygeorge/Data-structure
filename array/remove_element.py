0652393f396a8fa5afd8e32275cb9da6d3870f32
hcygeorge <hcygeorge>
2022-11-18 01:32:38 +0800

    add remove element

--- array/remove_element.py
+++ array/remove_element.py
@@ -1,14 +1,17 @@
 # Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed
 # hints
 # 元素順序無所謂
-# 直接交換元素位置會覆蓋到下個元素，而且無法判斷最後array的長度
+# 只用單紙張將元素與下個元素交換位置，將無法判斷移除後array的長度
 # 用快指針找到該留著的元素，移動元素到慢指針
+
+# 用快慢指針解
 class Solution(object):
     def removeElement(self, nums, val):
         """
         :type nums: List[int]
         :type val: int
         :rtype: int
+        
         """
         length = len(nums)
         fast = 0
@@ -21,3 +24,25 @@ class Solution(object):
             fast += 1
         
         return slow
+    
+# 用對撞指針解
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        length = len(nums)
+        left = 0
+        right = length
+        
+        while left < right:
+            if nums[left] == val:
+                nums[left] = nums[right-1]
+                right -= 1
+            else:
+                left += 1
+        
+        print(right, nums)
+        return right            
--- skill/two_pointer.md
+++ skill/two_pointer.md
@@ -4,13 +4,13 @@
 用兩個指針遍歷資料，按照指針移動的方向可分為對撞指針和快慢指針。
 
 ### 對撞指針(反方向)
-left, right兩個指針分別從資料兩端往中間移動
-- 可解squares of a sorted array
-- 可解two sum
+left, right兩個指針分別從資料兩端往中間移動，常用於sorted array題型，可解決
+- squares of a sorted array
+- two sum
+- remove element
 
 ### 快慢指針(同方向)
-兩個指針都從同方向開始移動，但慢指針只有在滿足條件下移動
-可解決
+兩個指針都從同方向開始移動，但慢指針只有在滿足條件下移動，可解決
 - sub sequence
 - remove duplicates from sorted array
 - remove element
