1c1d57f372e1b398cbbd58b20056bdc8f845c9fa
hcygeorge <hcygeorge>
2022-11-27 16:41:41 +0800

    add comments

--- array_easy/Best Time to Buy and Sell Stock II.py
+++ array_easy/Best Time to Buy and Sell Stock II.py
@@ -1,7 +1,10 @@
 # Best Time to Buy and Sell Stock II
+# You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+# On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+# Find and return the maximum profit you can achieve.
 
 # Simple One Pass
-# 只要明天價格比較高，就馬上賣策略
+# 只要明天價格比較高，就馬上賣
 # 因為沒有限制進出次數和進出代價，這個方法雖然在嚴格遞增時會有多餘的操作，但還是能得到正解
 class Solution(object):
     def maxProfit(self, prices):
