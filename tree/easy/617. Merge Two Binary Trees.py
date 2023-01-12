59cdc8ae707afea3bb37dd69120b5154a4ec688a
hcygeorge <hcygeorge@gmail.com>
2023-01-12 18:20:47 +0800

    add iter solution;

--- tree/easy/617. Merge Two Binary Trees.py
+++ tree/easy/617. Merge Two Binary Trees.py
@@ -25,3 +25,48 @@ class Solution(object):
         root1.right = self.mergeTrees(root1.right, root2.right)
 
         return root1
+    
+# 迭代解
+class Solution(object):
+    def mergeTrees(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: TreeNode
+        """
+        # 檢查root1, root2是否存在
+        if not root1 and not root2:
+            return None
+        if not root1 or not root2:
+            return root1 or root2
+        
+        # 也可以只檢查root1，root2是否存在留到後面while再檢查
+        # if not root1:
+        #     return root2
+        
+        stack = []
+        stack.append((root1, root2))
+
+        while stack:
+            t = stack.pop()
+            # 若t[1](root2)不存在不用合併
+            if not t[1]:
+                continue
+            # 以t[0](root1)為主體合併root2
+            t[0].val += t[1].val
+
+            # 同樣邏輯，先檢查root1子樹是否存在，若存在則加入stack做合併
+            if not t[0].left:
+                t[0].left = t[1].left
+            else:
+                stack.append((t[0].left, t[1].left)) 
+            # 同樣邏輯，先檢查root2子樹是否存在，若存在則加入stack做合併
+            if not t[0].right:
+                t[0].right = t[1].right
+            else:
+                stack.append((t[0].right, t[1].right)) 
+        
+        # 返回合併的root1
+        return root1
+
+
