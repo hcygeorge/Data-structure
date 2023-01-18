286c854a0f11fa5b73208aa74145494a0bc5f648
hcygeorge <hcygeorge@gmail.com>
2023-01-18 18:00:03 +0800

    add iteration solution

--- tree/medium/94. Binary Tree Inorder Traversal.py
+++ tree/medium/94. Binary Tree Inorder Traversal.py
@@ -8,7 +8,7 @@
 #         self.right = right
 
 # inorder: 左根右
-# 1.遞迴到最底下的左子樹後取得值
+# 1. 遞迴到最底下的左子樹後取得值
 # 2. 走回上一層取得其根節點的值
 # 3. 然後走到右子樹，以右子樹為根檢查1, 2
 # 4. 1,2都做完才取右子樹值
@@ -31,4 +31,31 @@ class Solution(object):
 
         inorder(root)
 
-        return self.res
+        return self.res  
+    
+# Iteration Solution
+# 建立res, stack
+# 1. cur從root開始，先一直往左子樹走，並將節點放入stack
+# 2. 走到底(左子樹不存在)後把stack的節點挑出來(後進的先出)，節點值加入res
+# 3. 走到右子樹，若右子樹存在則會回到1,2(往它的左子樹走)
+# 4. 若右子樹不存在，檢查條件會從stack挑出上一個左子樹(回到2)
+# 5. 重複以上步驟直到cur和stack都空了
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        stack = []  # LIFO
+        cur = root
+        while cur or stack:
+            if cur:
+                stack.append(cur)
+                cur = cur.left
+            else:
+                cur = stack.pop()
+                res.append(cur.val)
+                cur = cur.right
+        
+        return res
