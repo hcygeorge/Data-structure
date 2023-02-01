34f78fff133ebcfddd7bd6262b83ef56bdf118f7
hcygeorge <hcygeorge@gmail.com>
2023-02-01 17:58:55 +0800

    remaster

--- array/easy/283. Move Zeroes.py
+++ array/easy/283. Move Zeroes.py
@@ -1,24 +1,24 @@
 # Given an integer array nums, 
 # move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
-# use 2 pointers
-# 注意慢指針p2和p1同時前進，直到遇到0為止
+# 題目要求inplace，想到left right pointers
+# 慢指針負責指著最後一個0值等著與後面交換
 class Solution(object):
     def moveZeroes(self, nums):
         """
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        
-        p1 = 0
-        p2 = 0
-        
-        while p1 < len(nums):
-            if nums[p1] == 0:
-                p1 += 1
+        fast = 0
+        slow = 0
+
+        while fast < len(nums):
+            if nums[fast] == 0:
+                fast += 1
+                # 遇到0時，slow停著等著與後面的非0值交換
             else:
-                if p1 != p2:
-                    nums[p1], nums[p2] = nums[p2], nums[p1]
-                p1 += 1
-                p2 += 1
+                if fast != slow:  # 此時slow指向0而fast指著非0
+                    nums[fast], nums[slow] = nums[slow], nums[fast]
+                fast += 1
+                slow += 1  # 交換完畢，可以指向下一個值
                 
