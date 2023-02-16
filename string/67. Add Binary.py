1533574bc465834120ea7440e415f3183e9d2699
hcygeorge <hcygeorge@gmail.com>
2023-02-16 18:09:29 +0800

    add trick

--- string/67. Add Binary.py
+++ string/67. Add Binary.py
@@ -24,3 +24,12 @@ class Solution(object):
             idx_b -= 1
         
         return ''.join(res[::-1])
+
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        return bin(int(a, 2) + int(b, 2))[2:]
