bdcf653b57d3b8f634d0f8e06343670db1ef69bc
george <pch806771@ECONLINE.PRI>
2023-04-24 15:21:48 +0800

    update iteration solution

--- tree/easy/100. Same Tree.py
+++ tree/easy/100. Same Tree.py
@@ -5,6 +5,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution(object):
     def isSameTree(self, p, q):
         """
@@ -16,22 +17,8 @@ class Solution(object):
         if not p or not q: return False
         return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
-class Solution(object):
-    def isSameTree(self, p, q):
-        """
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: bool
-        """
-        if not p and not q: return True
-        if not p or not q: return False
-        
-        if p.val != q.val:
-            return False
-        else:
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
-
+# 思路: 將p,q存入deque中，對deque迭代，每次檢查完都先將p,q的座又子樹存入deque，以便繼續檢查
 from collections import deque
 class Solution(object):
     def isSameTree(self, p, q):
@@ -41,16 +28,23 @@ class Solution(object):
         :rtype: bool
         """
 
-        d = deque([(p, q)])
-        
-        while d:
-            pq = d.popleft()
-            if not pq[0] and not pq[1]: continue
-            if not pq[0] or not pq[1]: return False
+        d = deque([(p, q)])  # 雙向佇列，可以popleft()
 
-            print(pq[0].val,pq[1].val)
-            if pq[0].val == pq[1].val:
-                d.append((pq[0].left, pq[1].left))
-                d.append((pq[0].right, pq[1].right))
+        while d:
+            p, q = d.popleft()
+
+            # 確認存在性
+            if not p and not q:
+                continue
+            if not p or not q:
+                return False
+
+            # 檢查值是否相同
+            if p.val == q.val:
+                # 將下次要檢查的node排入deque
+                d.append((p.left, q.left))
+                d.append((p.right, q.right))
             else:
                 return False
+        # 通過全部檢查，代表兩樹相同
+        return True
