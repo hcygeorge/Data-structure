87fbf597e33c8bac355a4dad8d03931e32b2e62f
hcygeorge <hcygeorge>
2023-05-01 01:29:30 +0800

    note

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -1,6 +1,9 @@
-# 時間複雜度平均與最佳為O(nlogn)，若pivot選得不好導致兩個partition大小為1, n-1，則最差O(n^2)
+# 因為每次走訪數列的複雜度為n，需要遞迴次數k，
+# 每次遞迴n的長度減半，直到長度等於1，需要遞迴logn次(n/2^k = 1變成k = logn)
+# 因此時間複雜度平均與最佳為O(nlogn)
+# 若pivot選得不好導致兩個partition大小為1, n-1，則最差O(n^2)
 # 優點可以in place排序，空間複雜度O(1)
-# 
+
 def quick_sort(data: list, left: int, right: int):
     # end if length == 0
     if left >= right:
