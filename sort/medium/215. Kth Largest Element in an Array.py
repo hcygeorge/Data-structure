c09a5bfa52c0b0e3ed0b00fe75df8c6bc5847da1
hcygeorge <hcygeorge>
2023-04-30 22:18:25 +0800

    formatting

--- sort/medium/215. Kth Largest Element in an Array.py
+++ sort/medium/215. Kth Largest Element in an Array.py
@@ -1,12 +1,13 @@
 # Given an integer array nums and an integer k, return the kth largest element in the array.
 # Note that it is the kth largest element in the sorted order, not the kth distinct element.
 # You must solve it in O(n) time complexity.
+
 # 使用quick select方法，利用quick sort的partition概念將數列切成子數列
 # 其中一個收集小於pivot的數字，另一個則收集大的
 # 然後比較pivot位置和k-th的位置，判斷kth在哪個子數列內
 # 之後遞迴在子數列中繼續找kth
 # 與quick sort一樣，平均時間複雜度O(n)，最糟O(n^2)
-# 因為不用排序所有數字，所以實務上會比完整排序完再找kth快
+# 因為不用排序所有數字，所以實務上會比先完整排序再找kth快
 
 import random
 class Solution(object):
