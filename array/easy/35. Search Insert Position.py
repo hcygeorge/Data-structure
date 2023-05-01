36c7102bd1126b49d1650d4f732f66213dff7683
hcygeorge <hcygeorge>
2023-05-01 16:12:25 +0800

    second try

--- array/easy/35. Search Insert Position.py
+++ array/easy/35. Search Insert Position.py
@@ -1,9 +1,11 @@
 # Search Insert Position
 
-# 提示
+# tips
 # 使用binary search
 # 迭代條件必須包括lo=up
 # 當lo=up時，若仍未找到值，則lo會被加1，此時lo就是插入新值的位置
+
+# first try
 class Solution(object):
     def searchInsert(self, nums, target):
         """
@@ -25,5 +27,25 @@ class Solution(object):
         
         return lo
 
+# second try
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        lower, upper = 0, len(nums) - 1
+
+        while lower <= upper:
+            mid = (lower + upper) // 2
+            if nums[mid] < target:
+                lower = mid + 1
+            elif nums[mid] > target:
+                upper = mid - 1
+            else:
+                return mid
+        
+        return lower
 
 
