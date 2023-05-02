1fb7cca9123bd7320330268f37532521968cf3ef
george <pch806771@ECONLINE.PRI>
2023-05-02 18:34:33 +0800

    first try

--- sort/easy/88. Merge Sorted Array.py
+++ sort/easy/88. Merge Sorted Array.py
@@ -15,6 +15,7 @@
 # 注意nums2可能有剩下的元素(因為比較小所以沒有插入nums1)
 
 # blind spot:
+# 記得先檢查nums1, nums2存在性
 # 從左邊開始遍歷需要額外空間暫存較大的元素，無法處理nums2連續幾個元素都小於nums1[p1]的情況，交換後無法維持遞增狀態
 # 注意nums2可能有剩下的元素(因為比較小所以沒有插入nums1)，而nums1剩下因為已經遞增排序，無需處理
 
@@ -44,3 +45,31 @@ class Solution(object):
                     p2 += 1
 
             p1 += 1
+
+# first try
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        if not nums1 or not nums2:
+            return None
+        
+        p, p1, p2 = m+n-1, m-1, n-1
+        
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+            
+        # attach the rest of nums2 to the left of nums1
+        if p2 >= 0:
+            nums1[:p+1] = nums2[:p2+1]
