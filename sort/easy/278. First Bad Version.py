45a77d311e2321f5fca3030e81b50fd30f1a6f73
george <pch806771@ECONLINE.PRI>
2023-05-02 14:45:15 +0800

    second try and comments

--- sort/easy/278. First Bad Version.py
+++ sort/easy/278. First Bad Version.py
@@ -1,13 +1,15 @@
 # First Bad Version
 # You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
 # Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
-
 # The isBadVersion API is already defined for you.
 # @param version, an integer
 # @return a bool
 # def isBadVersion(version):
 
-# 資料有排序，可用binary search
+# tips:
+# 在有排序的數列尋找特定值的index，可用binary search
+
+# blind spot:
 # 注意version從1到n
 # 注意找到的bad version不一定是第一個，需要再檢查上一個是否為bad version
 
@@ -22,7 +24,6 @@ class Solution(object):
             if isBadVersion(ver):
                 return ver
             
-# Binary Search，時間複雜度O(logn)
 class Solution(object):
     def firstBadVersion(self, n):
         """
@@ -43,3 +44,23 @@ class Solution(object):
                 right = mid -1  # bad version在前面，更新上界
         
         return None  # 若找不到bad version
+    
+# second try: same as before
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        lower, upper = 1, n
+        
+        while lower <= upper:
+            mid = (lower+upper) // 2
+            if isBadVersion(mid):
+                if not isBadVersion(mid-1):
+                    return mid
+                upper = mid - 1
+            else:
+                lower = mid + 1
+
+        return None
