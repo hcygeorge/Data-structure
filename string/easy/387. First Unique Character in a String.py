fbf96fb3e1feb42564b35a960842014e8109930e
george <pch806771@ECONLINE.PRI>
2023-05-03 11:26:42 +0800

    add comments

--- string/easy/387. First Unique Character in a String.py
+++ string/easy/387. First Unique Character in a String.py
@@ -1,6 +1,12 @@
 # Given a string s, find the first non-repeating character in it and return its index.
 # If it does not exist, return -1.
 
+# tips:
+# 用dict累計出現次數
+
+# blind spot:
+# 因為英文只有26字母，空間複雜度是O(1)不是O(1)
+
 class Solution(object):
     def firstUniqChar(self, s):
         """
