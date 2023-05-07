58975594fddcf0fea3acbfbabebf31a49319c830
hcygeorge <hcygeorge>
2023-05-07 16:23:32 +0800

    first commit

---
+++ linked_list/easy/237. Delete Node in a Linked List.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        # 將現在的node取代下個node，沒有變數指向下個node指向的物件，GC會自動刪除該物件
+        node.val = node.next.val
+        node.next = node.next.next
+        
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        # 主動刪除下個node指向的物件
+        nextnode = node.next
+        node.val = nextnode.val
+        node.next = nextnode.next
+
+        del nextnode
