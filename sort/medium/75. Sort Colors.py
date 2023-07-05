868d70f32c97af1b9d4888192b638d395bbc20fb
george <pch806771@ECONLINE.PRI>
2023-07-05 15:46:30 +0800

    first commit

--- sort/medium/75. Sort Colors.py
+++ sort/medium/75. Sort Colors.py
@@ -1,24 +1,46 @@
 # tips: use counting sort, which means count the number of each digit, and then overwrite the array with ordered digits.
-
-from collections import OrderedDict
+# 用指針時要注意把數字2交換到後面時，指針p須留在原處檢查新數字是否為0
+# counting sort缺點是沒有穩定性
 class Solution(object):
     def sortColors(self, nums):
         """
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        counter = OrderedDict({0: 0, 1: 0, 2: 0})
+        counter = [0, 0, 0]  # idx represents the digit
         
+        # count digit
         for i in nums:
             counter[i] += 1
         
+        # replace digit in-place
         init = 0
-        for k, v in counter.items():
-            if v != 0:
-                for j in range(init, init+counter[k]):
-                    nums[j] = k
+        for num in range(3):
+            if counter[num] != 0:
+                for j in range(init, init+counter[num]):
+                    nums[j] = num
 
-                init += counter[k]
+                init += counter[num]
             
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        l = 0
+        r = len(nums) - 1
+        p = 0
+
+        while p <= r:
+            if nums[p] == 2:
+                nums[p], nums[r] = nums[r], nums[p]
+                r -= 1
+            elif nums[p] == 0:
+                nums[p], nums[l] = nums[l], nums[p]
+                p += 1
+            else:
+                p += 1
+
             
             
