9f43c7069507b7c5d468f10c8ad0b16038c9992d
george <pch806771@ECONLINE.PRI>
2023-07-07 11:26:38 +0800

    update node

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -1,8 +1,15 @@
-# 因為每次走訪數列的複雜度為n，需要遞迴次數k，
-# 每次遞迴n的長度減半，直到長度等於1，需要遞迴logn次(n/2^k = 1變成k = logn)
-# 因此時間複雜度平均與最佳為O(nlogn)
-# 若pivot選得不好導致兩個partition大小為1, n-1，則最差O(n^2)
+# 選擇一個pivot，然後用2 pointers從左右走訪數列
+# 當左邊大於pivot且右邊小於pivot時交換數字，直到2 pointers相遇
+# 交換pivot和2 pointers相遇點，此時pivot左邊的數字都小於pivot，右邊都大於pivot
+# 對pivot左右兩邊的數列重複剛才的步驟(遞迴)
 # 優點可以in place排序，空間複雜度O(1)
+
+# 複雜度說明
+# 因為每次走訪數列的複雜度為n，需要排序次數k，
+# 每次排序n的長度減半，重複排序直到長度等於1，需要排序logn次(n/2^k = 1變成k = logn)
+# 每次排序都會走過平均長度為n/2的數列
+# 因此時間複雜度為(n/2)xlogn = O(nlogn)
+# 若pivot選得不好導致兩個partition大小為1, n-1，則最差O(n^2)
 
 def quick_sort(data: list, left: int, right: int):
     # end if length == 0
@@ -25,7 +32,7 @@ def quick_sort(data: list, left: int, right: int):
         data[l], data[r] = data[r], data[l]
 
     # rank pivot
-    data[left] = data[l]
+    data[left] = data[l]  # because now all the numbers ahead of the pivot is low than the pivot
     data[l] = pivot
 
     # partition
